4398d1132e8fbec0f820965042c537f772b0cfd1
Jay9011 <whdtjq5904@gmail.com>
2025-11-20 11:09:39 +0900

    feat. KMS 초기 프로젝트 파일

---
+++ SECUiDEA_KMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SECUiDEA_KMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
